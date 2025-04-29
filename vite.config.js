c627eaf456d1cc2f7d95717dad9037a42e1fa2d7
stefano <stefano.cherio@edu.itspiemonte.it>
2025-04-29 13:08:38 +0000

    build changes

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,12 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-<<<<<<< HEAD
-            input: [ 'resources/js/app.jsx','resources/css/app.css','resources/css/modal.css'],
-=======
             input: [
                 "resources/js/app.jsx",
                 "resources/css/app.css",
                 "resources/css/modal.css",
             ],
->>>>>>> aea6915f2a51ed07f9d408e2cd8a8ea0b6176164
+
             refresh: true,
         }),
     ],
