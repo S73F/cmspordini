0cc31d99654931b547b6ad51c8925488a33cec1e
Stefano Cherio <scherioyt@gmail.com>
2025-04-29 15:18:18 +0200

    added modal.css into app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite(['resources/js/app.jsx', 'resources/css/app.css'])
+    @vite(['resources/js/app.jsx', 'resources/css/app.css', 'resources/css/modal.css'])
     @inertiaHead
 </head>
 
